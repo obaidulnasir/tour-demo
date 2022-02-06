6e68221ad20041e7d1c8a041ad6cc7c1cedb519b
obaidulnasir <obaidulnasir@outlook.com>
2022-02-06 13:06:56 +0600

    package added and fetch packages in umrah component

--- src/components/Dashboard/AddPackage/AddPackage.js
+++ src/components/Dashboard/AddPackage/AddPackage.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const AddPackage = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        fetch("http://localhost:5000/addPackage", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                alert("Package added successfully!!");
+                reset();
+            });
+    };
     return (
         <div>
-            <h3>Add Package</h3>
+            <Container>
+                <div className="row w-75 mx-auto">
+                    <h3>Add Package</h3>
+                    <br />
+                    <div className="col">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-group">
+                                <label>Package Name</label>
+                                <input
+                                    type="name"
+                                    className="form-control"
+                                    placeholder="Pakcage Name"
+                                    {...register("pakckageName")}
+                                />
+                                {errors.exampleRequired && <span>This field is required</span>}
+                            </div>
+                            <div className="form-group">
+                                <label>Price</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder=""
+                                    {...register("price")}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Image</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="image bb link"
+                                    {...register("img")}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Description</label>
+                                <textarea
+                                    class="form-control"
+                                    rows="3"
+                                    {...register("description")}
+                                ></textarea>
+                            </div>
+
+                            <input
+                                className="btn btn-info text-white my-3"
+                                type="submit"
+                                value="Add Pakcage"
+                            />
+                        </form>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
--- src/components/Packages/Umrah/UmrahPackges/UmrahPackges.js
+++ src/components/Packages/Umrah/UmrahPackges/UmrahPackges.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const UmrahPackges = () => {
     // ekhane data Fetch hobe 
+    const [umrahPac, setUmrahPac] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/allpackages")
+            .then(res => res.json())
+            .then(data => setUmrahPac(data));
+    }, [])
+
     return (
         <div>
+            <h2>{umrahPac.length}</h2>
             <h3>this is umrah packages</h3>
+
+            <Container>
+                <Row xs={1} md={3} className="g-4">
+                    {umrahPac.map((pac) => (
+                        <Col>
+                            <Card className="p-3">
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                    <Card.Title>{pac.pakckageName}</Card.Title>
+                                    <h4 className="fw-bold text-info">{pac.price} $</h4>
+                                    <Card.Text>
+                                        {pac.description.slice(0, 200)}...
+                                      </Card.Text>
+                                </Card.Body>
+                                <Link to="" className="text-center">
+                                    <button className="btn btn-info">More Details</button>
+                                </Link>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     );
 };
