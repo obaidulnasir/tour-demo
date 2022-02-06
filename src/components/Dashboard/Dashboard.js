108523aaf10932a9808a8d027967944bd9fd576f
obaidulnasir <obaidulnasir@outlook.com>
2022-02-06 11:51:03 +0600

    dashboard edited

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,18 +3,25 @@ import "./Dashboard.css"
 import useAuth from '../../hook/useAuth';
 
 const Dashboard = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     return (
         <div>
             <div className="text-center">
-            <h3>this is dashboard</h3>
-            <div>
-                <img className="avatar" src={user?.photoURL} alt="img" />
-            <h2>{user?.email}</h2>
-            </div>
-                
-                
-            <br /><br /><br /><br /><br /><br />
+                <h3>this is dashboard</h3>
+                <div>
+                    <img className="avatar" src={user?.photoURL} alt="img" />
+                    <h2>{user?.email}</h2>
+                </div>
+                <div>
+                    <div className="d-grid gap-2 d-md-block">
+                        <button className="btn btn-primary" type="button">My Order</button>
+                        <button className="btn btn-primary" type="button">Add Package</button>
+                    </div>
+                </div>
+                <hr />
+
+
+                <br /><br /><br /><br /><br /><br />
             </div>
         </div>
     );
